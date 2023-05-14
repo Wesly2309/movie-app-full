6561089c0e0247396d712960d030dfe2e7b51aa2
hjcodin <hansenjonatann@gmail.com>
2023-05-14 15:16:45 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Laravel Project</title>
+
+    <!-- Stylesheet -->
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-slate-900">
+    <!-- Your content -->
+    @yield('content')
+</body>
+
+</html>
