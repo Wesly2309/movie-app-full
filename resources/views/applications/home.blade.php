5014e1837a9fa70d4d737b9f30a32c7540abc98e
Wesly2309 <weslylims02@gmail.com>
2023-05-15 20:11:43 +0700

    second commit

--- resources/views/applications/home.blade.php
+++ resources/views/applications/home.blade.php
@@ -7,10 +7,10 @@
             <div class="grid grid-cols-4 gap-8">
                 <div class="mt-8">
                     <a href="#">
-                        <img src="asset/img/once-we-get-married-poster.jpeg" alt="Once We Get Married">
+                        <img src="/img/demonslayer.jpg" alt="Demon Slayer">
                     </a>
                     <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 text-white hover:text-gray-300">Once We Get Married</a>
+                        <a href="#" class="text-lg mt-2 text-white hover:text-gray-300">Demon Slayer: Kimetsu No Yaiba - To the Swordsmith Village</a>
                         <div class="flex items-center text-gray-400">
                             <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24">
                                 <g data-name="
@@ -23,16 +23,46 @@
                                         data-name="star" />
                                     <g>
                             </svg>
-                            <span class="ml-1">85%</span>
+                            <span class="ml-1">6.7</span>
                             <span class="mx-2">|</span>
-                            <span>14 Mei 2023</span>
-
+                            <span>9 April 2023</span>
+                            <div class="text-gray-400 text-sm">
+                                Action, Adventure, Fantasy, Sci-Fi
+                            </div>
                         </div>
                     </div>
+                    
                 </div>
-
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/spiderman.jpg" alt="Spider Man">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 text-white hover:text-gray-300">Spider-Man: No Way Home</a>
+                        <div class="flex items-center text-gray-400">
+                            <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24">
+                                <g data-name="
+                                Layer 2">
+                                    <path
+                                        d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.671a1 1 0
+                                01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1
+                                0 011.8 0l2.54 5.12 5.7.83a1 1 0 0f1.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0
+                                01-.4 1 1 1 0 01-.62.18z"
+                                        data-name="star" />
+                                    <g>
+                            </svg>
+                            <span class="ml-1">8.2</span>
+                            <span class="mx-2">|</span>
+                            <span>15 December 2021</span>
+                            <div class="text-gray-400 text-sm">
+                                Action, Adventure, Fantasy, Sci-Fi
+                            </div>
+                        </div>
+                    </div>
+                    </div> <!-- end popular movies -->
+                    
+                </div>
             </div>
-
         </div>
     </div>
 @endsection
---
+++ resources/views/layouts/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="movie info border-b border-gray-800">
+        <div class="container mx-auto px-4 py-16 flex">
+            <img src="/img/demonslayer.jpg" alt="Demon Slayer" class="w-96" style="width: 24rem">
+            <div class="ml-24">
+                Details here
+            </div>
+        </div>
+        
+    </div> <!-- end movie -->
+@endsection
