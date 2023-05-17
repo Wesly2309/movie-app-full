399f89394475e9dbe00d0b9b3eb08815bd717492
Wesly2309 <weslylims02@gmail.com>
2023-05-17 19:02:47 +0700

    seven commit

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\MoviesController;
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'app');
 Route::view('/movie', 'layouts.show');
 
 Route::get('/', [HomeController::class , 'index']);
+Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
+Route::get('/movies/{movie}', [MoviesController::class, 'show'])->name('movies.show');
 
 
