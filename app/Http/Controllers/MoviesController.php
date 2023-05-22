013c83377810e52e0ff823193522ce42b6b01697
hjcodin <hansenjonatann@gmail.com>
2023-05-22 19:53:43 +0700

    first commit in dev branch

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -14,15 +14,16 @@ class MoviesController extends Controller
     {
         $popularMovies = Http::withToken(config('services.tmdb.token'))
             ->get('https://api.themoviedb.org/3/movie/popular')
-            ->json()['results'];
+            ->json()["results"];
 
+        
         $nowPlayingMovies = Http::withToken(config('services.tmdb.token'))
             ->get('https://api.themoviedb.org/3/movie/popular')
-            ->json()['results'];
+            ->json()["results"];
 
         $genresArray = Http::withToken(config('services.tmdb.token'))
             ->get('https://api.themoviedb.org/3/genre/movie/list')
-            ->json()['genres'];
+            ->json()["genres"];
 
         $genres = collect($genresArray)->mapwithKeys(function ($genre) {
             return [$genre['id'] => $genre['name']];
